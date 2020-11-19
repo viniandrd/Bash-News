594d84398445e4645352b2fa9cace08fc252b995
Vinicius Andrade <viniandrd@hotmail.com>
2020-11-19 09:21:20 -0300

    Criando os PDF's a partir de um template

--- buildPDF.py
+++ buildPDF.py
@@ -1,7 +1,6 @@
 import fpdf
 from fpdf import FPDF
-import os
-fpdf.set_global("SYSTEM_TTFONTS", os.path.join(os.path.dirname(__file__),'fonts'))
+fpdf.set_global("SYSTEM_TTFONTS", './fonts')
 
 header_title = None
 header_date = None
@@ -28,13 +27,14 @@ class PDF(FPDF):
         self.image('./images/Logo.png', 10, 8, 33)
         self.set_font('Arial', 'B', 15)
         # Add an address
-        self.cell(100)
-        self.cell(0, 5, header_title, ln=1)
-        self.cell(100)
-        self.cell(0, 5, header_date, ln=1)
-        self.cell(100)
+        self.cell(60)
+        self.cell(0, 7, 'O melhor buscador de notícias!', ln=1)
+        #self.cell(100)
+        #self.cell(0, 2, header_date, ln=1)
+        #self.cell(100)
         #self.cell(0, 5, 'Any Town, USA', ln=1)
-
+        self.ln(20)
+        self.line(10, 36, 200, 36)
         # Line break
         self.ln(20)
 
@@ -56,11 +56,19 @@ class PDF(FPDF):
         self.ln(4)
 
     def content(self, idx, list):
-        # Read text file
-        '''with open(summary_file, 'r') as f:
-            txt = f.read()
-        print(txt)'''
-        # Times 12
+        # Arial bold 15
+        self.cell(40)
+        self.set_font('Arial', 'B', 16)
+        # Output the Title
+        self.multi_cell(120, 5, header_title, align='C')
+        #self.cell(0, 7, header_title, ln=1)
+        self.cell(10)
+        #self.cell(0, 2, header_date, ln=1)
+        self.cell(10)
+        # Line break
+        self.ln(5)
+
+        # NotoSans 14 for the summary
         self.set_font('NotoSans', '', 14)
         # Output justified text
         print(list[idx]['Summary'])
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from __future__ import print_function
 from GoogleNews import GoogleNews
-from newspaper import Article
-from newspaper import Config
-import pandas as pd
-import nltk, getopt, sys, argparse, pdfkit, time
-from pprint import pprint
+from newspaper import Article, Config
 from buildPDF import create_pdf
+from datetime import date
+import pandas as pd
+import nltk, argparse
+
 
 nltk.download('punkt')
 
@@ -15,9 +14,7 @@ config.browser_user_agent = user_agent
 
 def search(keyword=None, datestart=None, dateend=None):
     #Parametros de busca
-    print('Key: ', keyword)
-    print('Date start: ', datestart)
-    print('Date end: ', dateend)
+    print('Keyword: ', keyword)
 
     #Configuração da pesquisa
     googlenews = GoogleNews(start=datestart, end=dateend)
@@ -33,47 +30,29 @@ def search(keyword=None, datestart=None, dateend=None):
 
     #Pega um range de páginas obtidas do resultado acima
     for i in range(0,1):
-        print('idx', i)
         googlenews.getpage(i)
         result = googlenews.result()
         df = pd.DataFrame(result)
 
     list = []
 
-    print('idxs: ', df.index)
-
-    formated_datestart = datestart.split("/")
-    datestart_save = formated_datestart[0] + '-' + formated_datestart[1] + '-' + formated_datestart[2]
-
-    formated_dateend = dateend.split("/")
-    dateend_save = formated_dateend[0] + '-' + formated_dateend[1] + '-' + formated_dateend[2]
-
-    # --- Fazendo testes com arquivos de texto
-    txtfilename = "./txts/sumario.txt"
-
-    # --- Salvando as informações das noticias (summary) em um arquivo de texto
-    txt = open(txtfilename, 'w')
     for ind in df.index:
-        print(ind)
+        print('Noticia numero: {}'.format(ind))
         dict = {}
         article = Article(df['link'][ind], config=config)
         article.download()
-        #try:
-        article.parse()
-        article.nlp()
-        dict['Date'] = df['date'][ind]
-        dict['Media'] = df['media'][ind]
-        dict['Title'] = article.title
-        dict['Article'] = article.text
-        dict['Summary'] = article.summary
-        list.append(dict)
-        txt.write(article.summary)
-        #print(list[ind]['Summary'])
-        #txt.write(list[ind])
-        create_pdf(ind, list)
-        #except:
-        #    print('error')
-    txt.close()
+        try:
+            article.parse()
+            article.nlp()
+            dict['Date'] = df['date'][ind]
+            dict['Media'] = df['media'][ind]
+            dict['Title'] = article.title
+            dict['Article'] = article.text
+            dict['Summary'] = article.summary
+            list.append(dict)
+            create_pdf(ind, list)
+        except:
+            print('Error')
 
     #--------Convertendo o DataFrame pra um arquivo do Excel
     #news_df = pd.DataFrame(list)
@@ -81,22 +60,28 @@ def search(keyword=None, datestart=None, dateend=None):
 
 
 if __name__ == '__main__':
-    #Caso for testar o código na IDE, definir os parametros de busca aqui.
-    keyword = 'NBA'
-    initialdate = '10/12/2020'
-    finaldate = '10/12/2020'
+    # Pega a data atual para busca de notícias
+    today = date.today()
+    today = str(today)
+    separa = today.split("-")
+    dia = separa[2]
+    mes = separa[1]
+    ano = separa[0]
+    data_atual = '{}/{}/{}'.format(mes, dia, ano)
+
+    # Caso for testar o código na IDE, definir os parametros de busca aqui.
+    keyword = 'Corona Virus'
+    initialdate = data_atual
+    finaldate = data_atual
+
 
     #Pegando as informações da linha de comando (nao necessario caso for rodar direto na IDE)
     parser = argparse.ArgumentParser()
-    parser.add_argument("-key", "--keyword", help="Database name")
-    parser.add_argument("-dts", "--datestart", help="Initial Date")
-    parser.add_argument("-dte", "--dateend", help="Final Date")
+    parser.add_argument("-key", "--keyword", help="Keyword")
 
-    if keyword == None and initialdate == None and finaldate == None:
+    if keyword == None:
         args = parser.parse_args()
         keyword = str(args.keyword)
-        initialdate = str(args.datestart)
-        finaldate = str(args.dateend)
 
     #Faz a busca pelas notícias
     search(keyword, initialdate, finaldate)
